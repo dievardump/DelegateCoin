6b5a3a412838ae0cce7e02426e2241baefc4dd29
Simon Fremaux <dievardump@gmail.com>
2020-12-26 21:48:42 +0100

    shows that storage are not mixed

--- test/1_FullCoin.js
+++ test/1_FullCoin.js
@@ -6,8 +6,13 @@ contract('FullCoin', async (accounts) => {
     instance = await FullCoin.deployed();
 	});
 
-  it('accounts 0 should have 1337', async () => {
+  it('account 0 should have 1337', async () => {
 		const balance = await instance.balanceOf(accounts[0]);
 		assert.equal(balance.valueOf(), 1337, "Wrong balance - should be 1337");
+  });
+
+	it('account 1 should have 0', async () => {
+		const balance = await instance.balanceOf(accounts[1]);
+		assert.equal(balance.valueOf(), 0, "Wrong balance - should be 0");
   });
 });
--- test/2_DelegateCoin.js
+++ test/2_DelegateCoin.js
@@ -12,4 +12,9 @@ contract('DelegateCoin', async (accounts) => {
 		const balance = await instance.balanceOf(accounts[1]);
 		assert.equal(balance.valueOf(), 7331, "Wrong balance - should be 7331");
   });
+
+	it('account 0 should have 0', async () => {
+		const balance = await instance.balanceOf(accounts[0]);
+		assert.equal(balance.valueOf(), 0, "Wrong balance - should be 0");
+  });
 });
